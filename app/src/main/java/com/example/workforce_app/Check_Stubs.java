2365f973a59327794a7cfac2d50780e130d0e40b
mathews1193 <mathews1193@yahoo.com>
2020-09-06 23:57:48 -0500

    created pages from dashboard

---
+++ app/src/main/java/com/example/workforce_app/Check_Stubs.java
@@ -0,0 +1,14 @@
+package com.example.workforce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Check_Stubs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check__stubs);
+    }
+}
---
+++ app/src/main/java/com/example/workforce_app/Employee_Information.java
@@ -0,0 +1,14 @@
+package com.example.workforce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Employee_Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee__information);
+    }
+}
